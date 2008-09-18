c1bdcc3ce57c603c14b6eb423b12137ca53d10e1
Jonathan.Wage <Jonathan.Wage@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-09-18 10:50:44 -0500

    [1.2] Initial entry of sfDoctrinePlugin tests
    git-svn-id: http://svn.symfony-project.com/plugins/sfDoctrinePlugin/trunk@11650 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ test/unit/sfDoctrineDatabaseTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once(dirname(__FILE__).'/../bootstrap/unit.php');
+
+$t = new lime_test(1, new lime_output_color());
+
+$parameters = array(
+    'name'       => 'doctrine',
+    'dsn'        => 'sqlite::memory');
+
+$p = new sfDoctrineDatabase($parameters);
+$t->is($p->getDoctrineConnection()->getName(), 'doctrine', 'initialize() - creates a valid doctrine configuration from parameters');
