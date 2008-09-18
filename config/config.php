54edb2d30e1aa0419da90f4f4af482f9d2581656
Jonathan.Wage <Jonathan.Wage@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-09-17 21:33:11 -0500

    [1.2] Adding condition around config.php contents so it is only executed if doctrine is set as the default orm
    git-svn-id: http://svn.symfony-project.com/plugins/sfDoctrinePlugin/trunk@11634 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- config/config.php
+++ config/config.php
@@ -1,7 +1,10 @@
 <?php
-if (sfConfig::get('sf_web_debug'))
+if (sfConfig::get('sf_orm') == 'doctrine')
 {
-  require_once dirname(__FILE__).'/../lib/debug/sfWebDebugPanelDoctrine.class.php';
+  if (sfConfig::get('sf_web_debug'))
+  {
+    require_once dirname(__FILE__).'/../lib/debug/sfWebDebugPanelDoctrine.class.php';
 
-  $this->dispatcher->connect('debug.web.load_panels', array('sfWebDebugPanelDoctrine', 'listenToAddPanelEvent'));
+    $this->dispatcher->connect('debug.web.load_panels', array('sfWebDebugPanelDoctrine', 'listenToAddPanelEvent'));
+  }
 }
