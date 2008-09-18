8aef481541bc0b44c4f24569915648dfc6bac582
Jonathan.Wage <Jonathan.Wage@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-09-18 11:33:23 -0500

    [1.2] Adding method for configuring Doctrine via the configuration classes
    git-svn-id: http://svn.symfony-project.com/plugins/sfDoctrinePlugin/trunk@11654 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/addon/sfDoctrineAutoload.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ * (c) Jonathan H. Wage <jonwage@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Autoloading and initialization for doctrine.
+ *
+ * @package    symfony
+ * @subpackage doctrine
+ * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ * @author     Jonathan H. Wage <jonwage@gmail.com>
+ * @version    SVN: $Id: sfDoctrineAutoload.php 9122 2008-05-20 23:18:21Z dwhittle $
+ */
+
+require_once(dirname(__FILE__) . '/../doctrine/Doctrine.php');
+
+if (method_exists(sfProjectConfiguration::getActive(), 'configureDoctrine'))
+{
+  sfProjectConfiguration::getActive()->configureDoctrine();
+}
